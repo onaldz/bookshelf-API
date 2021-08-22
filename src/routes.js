8b45bb6f0561f525500867859f38fb7acf316348
Ronal Lumowa <onaldlumowa@gmail.com>
2021-08-23 00:49:31 +0800

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+const { addBookHandler } = require('./handler');
+const { getAllBooksHandler } = require('./handler');
+const { getBookByIdHandler } = require('./handler');
+const { editBookByIdHandler, deleteBookByIdHandler } = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookByIdHandler,
+  },
+  {
+    method: '*',
+    path: '/{any*}',
+    handler: () => 'Halaman tidak ditemukan',
+  },
+];
+
+module.exports = routes;
